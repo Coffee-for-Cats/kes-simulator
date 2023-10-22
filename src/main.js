fa326c341b9a310ef795cca14fd6eb89243706bd
Lucas S. Silva <57202598+Coffee-for-Cats@users.noreply.github.com>
2023-10-22 00:46:48 -0300

    modified controls, speed, mark behavior and font

--- src/main.js
+++ src/main.js
@@ -12,17 +12,22 @@ async function execute() {
   }
 }
 
-const speed = 800;
-function startWalking() {
+let playing = false;
+const speed = document.getElementById("speed");
+function play() {
+  playing = !playing;
   if (RC > 31) RC = 0;
   walk();
 }
 
+const delayUpperLimit = 2000;
 function walk() {
+  if (!playing) return;
   mainMemory = getAssemblyLines();
   step();
   if (RC <= 31) {
-    window.setTimeout(walk, speed)
+    const delay = (-1 * speed.value) + delayUpperLimit;
+    window.setTimeout(walk, delay)
   }
 }
 
